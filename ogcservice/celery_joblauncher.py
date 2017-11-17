d698e83cab484adad4a0c0f7a80db7369eb62a3f
fderue <francois.xavier.derue@gmail.com>
2017-11-17 12:28:19 -0500

    add task_id as input_data and read json_file output for result

--- ogcservice/celery_joblauncher.py
+++ ogcservice/celery_joblauncher.py
@@ -3,6 +3,7 @@ import os
 import time
 from celery_request import Request
 from celery.utils.log import get_task_logger
+import json
 
 
 PROCESS_NAME = 'worker.joblauncher'
@@ -59,6 +60,8 @@ def task_joblauncher(self, args):
     if not request.volume_mapping:
         request.volume_mapping = volume_mapping
 
+    request.body['input_data']['task_id'] = task_id
+
     #Start processing
     request.set_progress(0)
     error_code = run_image(request)
@@ -68,7 +71,16 @@ def task_joblauncher(self, args):
     time.sleep(20)  # Fake progression
     request.set_progress(100)
 
-    return {'result': {'output_url': 'url_to_receive_from_application'}}
+    json_output_file = '/tmp/ogc/tasks/{uuid}/outputs/{uuid}.json'.format(uuid=task_id)
+    if os.path.exists(json_output_file):
+        json_output = json.load(open(json_output_file))
+    else:
+        json_output = {'outputs':'output_from_application'}
+    # Get output url
+    # output_data_url: http://10.1.35.59/fs/tasks/6b0d3c64-5978-483c-9fc3-f37cdb46c821/outputs/outputs/generate_dem_test/DEM_RS2_OK79000_PK698379_DK627315_FQ9W_20160907_013546_HH_VV_HV_VH_SLC_worker2.tif
+    # output_data_WMS_url: http://132.217.140.40:8080/geoserver/OGC01/wms?service=WMS&version=1.1.0&request=GetMap&layers=OGC01:RS2_OK79000_PK698379_DK627315_FQ9W_20160907_013546_HH_VV_HV_VH_SLC-2017-08-02T20:30:46.818016Z&styles=&bbox=-118.93031183853228,49.834373782054,-118.15157741042583,50.228387801408914&width=768&height=388&srs=EPSG:4326&format=image%2Fpng&bgcolor=000000
+
+    return {'result': json_output}
 
 
 
